163c2a0d02ffe57ad67cfdd396ab19a9b57f47a2
TheEmeraldRanger <TheEmeraldRanger@users.noreply.github.com>
2019-06-30 12:17:57 -0400

    Update and rename character-creation.js to charactercards.js

--- character-creation.js
+++
@@ -1,5 +0,0 @@
-var player1 = document.getElementById("p1")
-var player2 = document.getElementById("p2")
-var player3 = document.getElementById("p3")
-var player4 = document.getElementById("p4")
-var guide = document.getElementById("npc-g")
---
+++ charactercards.js
@@ -0,0 +1,4 @@
+var player1 = document.getElementById("playercard1")
+var addpcbutton = document.getElementById("addplayercard")
+
+
